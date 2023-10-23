0b01984404310dd2d72090e836d7fc97ecac6bd9
Vincent Molin <molinv@chalmers.se>
2023-10-23 10:53:57 +0200

    fix tests in gpu env

--- pdmpx/poisson_time/linear_test.py
+++ pdmpx/poisson_time/linear_test.py
@@ -10,24 +10,25 @@ def allclose(a, b):
 
 def test_ab_poisson_time():
     us = jr.uniform(jr.key(0), (5,))
+    cus = np.array(us)
 
     a, b = 1.0, -2.0
     assert allclose(
-        cpu_ab_poisson_time(us[0], a, b), linear.ab_poisson_time(us[0], a, b)
+        cpu_ab_poisson_time(cus[0], a, b), linear.ab_poisson_time(us[0], a, b)
     )
 
     a, b = 0.0, -2.0
     assert allclose(
-        cpu_ab_poisson_time(us[1], a, b), linear.ab_poisson_time(us[1], a, b)
+        cpu_ab_poisson_time(cus[1], a, b), linear.ab_poisson_time(us[1], a, b)
     )
 
     a, b = 2.0, 0.0
-    assert allclose(-np.log(us[2]) / a, linear.ab_poisson_time(us[2], a, b))
+    assert allclose(-np.log(cus[2]) / a, linear.ab_poisson_time(us[2], a, b))
 
     a, b = 1.0, -0.0
-    assert allclose(-np.log(us[3]) / a, linear.ab_poisson_time(us[3], a, b))
+    assert allclose(-np.log(cus[3]) / a, linear.ab_poisson_time(us[3], a, b))
 
     a, b = 10.0, 1.0
     assert allclose(
-        cpu_ab_poisson_time(us[4], a, b), linear.ab_poisson_time(us[4], a, b)
+        cpu_ab_poisson_time(cus[4], a, b), linear.ab_poisson_time(us[4], a, b)
     )
