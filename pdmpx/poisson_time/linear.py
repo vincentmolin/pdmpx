7642fd1aa97f08bceb6a2acd297b4516a159aea6
Vincent Molin <molinv@chalmers.se>
2023-10-25 02:07:09 +0200

    might solve some edge cases

--- pdmpx/poisson_time/linear.py
+++ pdmpx/poisson_time/linear.py
@@ -2,29 +2,29 @@ import jax
 import jax.numpy as jnp
 
 
-@jax.jit
 def ab_poisson_time(u: float, a: float, b: float) -> float:
     """
     First arrival time of Poisson process with time-dependent rate `rate = (a + b*t)+`
     """
+    y = -jnp.log(1.0 - u)
     return jax.lax.select(
         b < 0,
         jax.lax.select(
             a <= 0,
             jnp.inf,
             jax.lax.select(
-                -a / b < -jnp.log(u),
-                (-a - jnp.sqrt(2 * b * -jnp.log(u) + a**2)) / b,
+                -a / b < y,
+                (-a - jnp.sqrt(2 * b * y + a**2)) / b,
                 jnp.inf,
             ),
         ),
         jax.lax.select(
             b == 0,
-            jax.lax.select(a <= 0, jnp.inf, -jnp.log(u) / a),
+            jax.lax.select(a <= 0, jnp.inf, y / a),
             jax.lax.select(
                 a < 0,
-                (-a + jnp.sqrt(2 * b * -jnp.log(u))) / b,
-                (-a + jnp.sqrt(2 * b * -jnp.log(u) + a**2)) / b,
+                (-a + jnp.sqrt(2 * b * y)) / b,
+                (-a + jnp.sqrt(2 * b * y + a**2)) / b,
             ),
         ),
     )
